7259cca8cc8f02923e785dadabc087e7303bbcde
ayegbakenneth <ayegbakenneth@gmail.com>
2024-11-08 08:36:56 +0100

    Python module second assignment.

--- list.py
+++ list.py
@@ -1,9 +1,10 @@
-# Create an empty list called my_list.
+# Python Module Second Assignment
+# 1. Create an empty list called my_list.
 
 my_list = []
 print(my_list)
 
-# Append the following elements to my_list: 10, 20, 30, 40.
+# 2. Append the following elements to my_list: 10, 20, 30, 40.
 
 my_list.append(10)
 my_list.append(20)
@@ -12,28 +13,28 @@ my_list.append(40)
 
 print(my_list)
 
-# Insert the value 15 at the second position in the list.
+# 3. Insert the value 15 at the second position in the list.
 
 my_list.insert(2, 15)
 print(my_list)
 
-# Extend my_list with another list: [50, 60, 70].
+# 4. Extend my_list with another list: [50, 60, 70].
 
 another_list = [50, 60, 70]
 my_list.extend(another_list)
 print(my_list)
 
-# Remove the last element from my_list.
+# 5. Remove the last element from my_list.
 
 my_list.remove(70)
 print(my_list)
 
-# Sort my_list in ascending order.
+# 6. Sort my_list in ascending order.
 
 my_list.sort()
 print(my_list)
 
-# Find and print the index of the value 30 in my_list.
+# 7. Find and print the index of the value 30 in my_list.
 
 index_of_30 = my_list.index(30)
 print(index_of_30)
